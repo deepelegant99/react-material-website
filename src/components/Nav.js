7dbca9fe7030c62036c9dcac9c1ad5d506b84ba0
Henry Crossley <crossh2001@yahoo.com>
2021-09-15 00:25:07 -0700

    made the app bar longer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,6 +12,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    width: 1360
   },
   menuButton: {
     marginRight: theme.spacing(2),
